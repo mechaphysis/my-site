03c0beca30b74cd279e0658ea1abf4cfa6c67b2a
Esperanza González <esperanza@mechaphysis.org>
2019-07-01 20:19:10 +0200

    feat: add basic graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Esperanza's Portolio",
+    author: 'Esperanza Natalia González'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ]
