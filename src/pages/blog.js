e366d1f107461945d0bcbe5333c669c0609cdbf4
Esperanza González <esperanza@mechaphysis.org>
2019-07-01 23:33:57 +0200

    feat: style posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,8 @@ import Layout from '../components/layout';
 import { graphql, useStaticQuery } from 'gatsby';
 import { Link } from 'gatsby'
 
+import blogStyles from './blog.module.scss'
+
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
         query {
@@ -26,11 +28,11 @@ const BlogPage = () => {
         <Layout>
             <h1>Blog</h1>
             <p>Posts will show up here later</p>
-            <ol>
+            <ol className={blogStyles.posts}>
                 {
                     data.allMarkdownRemark.edges.map( edge => {
                         return(
-                            <li>
+                            <li className={blogStyles.post}>
                                 <Link to={`/blog/${edge.node.fields.slug}`}>
                                     <h2>{edge.node.frontmatter.title}</h2>
                                     <p>{edge.node.frontmatter.date}</p>
