d4ff4c2358457b4063000701d2e3db0062226452
Esperanza González <esperanza@mechaphysis.org>
2019-07-01 22:32:25 +0200

    feat: generate dynamically links for posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout';
 import { graphql, useStaticQuery } from 'gatsby';
+import { Link } from 'gatsby'
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
@@ -11,6 +12,9 @@ const BlogPage = () => {
                         frontmatter {
                             title
                             date
+                        }
+                        fields {
+                            slug
                         }
                     }
                 }
@@ -27,8 +31,10 @@ const BlogPage = () => {
                     data.allMarkdownRemark.edges.map( edge => {
                         return(
                             <li>
-                                <h2>{edge.node.frontmatter.title}</h2>
-                                <p>{edge.node.frontmatter.date}</p>
+                                <Link to={`/blog/${edge.node.fields.slug}`}>
+                                    <h2>{edge.node.frontmatter.title}</h2>
+                                    <p>{edge.node.frontmatter.date}</p>
+                                </Link>
                             </li>
                         )
                     })
