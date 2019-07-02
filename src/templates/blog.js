dfdd3ca26f5bc03a6ecad845b737ce69f9c3f24e
Esperanza González <esperanza@mechaphysis.org>
2019-07-02 23:36:28 +0200

    feat: adjust blog template to accomodate for contentful data

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout"
 
@@ -7,27 +8,21 @@ export const query = graphql`
     query (
         $slug: String!
     ) {
-        markdownRemark (
-        fields: {
-            slug: {
-            eq: $slug
-            }
-        }
-        ) {
-            frontmatter {
+        contentfulBlogPost(slug: {eq: $slug}) {
             title
-            date
-        }
-        html
+            publishedDate(fromNow: true)
+            body {
+                json
+            }
         }
     }
 `
 const Blog = (props ) => {
     return (
         <Layout>
-            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-            <p>{props.data.markdownRemark.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div>
+            <h1>{props.data.contentfulBlogPost.title}</h1>
+            <p>{props.data.contentfulBlogPost.publishedDate}</p>
+            {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
         </Layout>
     )
 }
