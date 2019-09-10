bef6404bc9da495167dbd578f16e792bbce9de59
Esperanza González <esperanza@mechaphysis.org>
2019-09-10 19:58:50 +0200

    feat: test custom parameters for pixel

--- src/components/Pixel/PixelViewContent.js
+++ src/components/Pixel/PixelViewContent.js
@@ -3,7 +3,10 @@ import React, {Fragment} from "react"
 export default () =>
     <Fragment>
         <script dangerouslySetInnerHTML={{ 
-            __html: `fbq('track', 'ViewContent');`
+            __html: `fbq('track', 'ViewContent', {
+                content_name: 'A simple gatsby static blog',
+                content_category: 'Blogs > Technology'
+            });`
             
         }}/>    
         </Fragment>
