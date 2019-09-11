5f3ced7243511b66abc2d27bebbb69b7c2e66da5
Esperanza González <esperanza@mechaphysis.org>
2019-09-11 18:40:34 +0200

    fix: restore datalayer

--- src/components/head.js
+++ src/components/head.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql} from "gatsby"
 import TagManagerBase from "./TagManager/TagManagerBase"
+import DataLayer from "./TagManager/DataLayer"
 
 const Head = ({ title }) => {
     const data = useStaticQuery(graphql`
@@ -15,6 +16,7 @@ const Head = ({ title }) => {
     `)
     return(
         <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
+            <DataLayer></DataLayer>
             <TagManagerBase></TagManagerBase>
         </Helmet>
     )
