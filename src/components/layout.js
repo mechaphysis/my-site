f200c2bc5980f2d7b106e747d1bb14da54ed94f6
Esperanza González <esperanza@mechaphysis.org>
2019-09-11 16:36:47 +0200

    feat: install google tag manager in head and body

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,11 +5,13 @@ import Footer from "./footer";
 import '../styles/index.scss'
 import layoutStyles from './layout.module.scss'
 import PixelViewContent from "./Pixel/PixelViewContent";
+import TagManagerNoScript from "./TagManager/TagManagerNoScript";
 
 const Layout = (props) => {
 
     return (
         <body>
+            <TagManagerNoScript></TagManagerNoScript>
             <PixelViewContent></PixelViewContent>
             <Header />
             <div className={layoutStyles.container}>
