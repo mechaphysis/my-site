b1680e027a456c09a0f27314635e104dccf1a375
Esperanza González <esperanza@mechaphysis.org>
2019-09-13 17:12:13 +0200

    chore: remove unused segment event

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -9,11 +9,7 @@ const PortfolioPage = () => {
         <Layout>
             <Head title="Craftings"/>
             <h1>List of Project goes here</h1>
-            <button onClick={
-                () => analytics.track("Add to Cart", {
-                    item: "Lorem Ipsum", 
-                    accountType: "High-value customer"
-                    })}>Add to cart</button>
+            <button>Add to cart</button>
         </Layout>
     )
 }
